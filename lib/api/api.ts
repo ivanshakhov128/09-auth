4e2e2e42075c0b33184597d42c1acae42ba4ba70
Ivan Shakhov <ivanshahov04@gmail.com>
2026-01-02 22:47:57 +0200

    six

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-// Берем URL из переменной окружения
 const baseURL = process.env.NEXT_PUBLIC_API_URL + "/api";
 
-// Общий экземпляр axios для клиента и сервера
 export const api = axios.create({
   baseURL,
-  withCredentials: true, // важно для авторизации через куки
+  withCredentials: true,
 });
--- proxy.ts
+++ proxy.ts
@@ -1,22 +1,16 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-/**
- * Проверка приватного маршрута
- */
 export async function protectPrivateRoute() {
-  const cookieStore = await cookies(); // ✅ добавляем await
+  const cookieStore = await cookies();
   const sessionCookie = cookieStore.get("notehub-session");
   if (!sessionCookie) {
     redirect("/sign-in");
   }
 }
 
-/**
- * Проверка публичного маршрута (sign-in / sign-up)
- */
 export async function protectPublicRoute() {
-  const cookieStore = await cookies(); // ✅ добавляем await
+  const cookieStore = await cookies();
   const sessionCookie = cookieStore.get("notehub-session");
   if (sessionCookie) {
     redirect("/profile");
